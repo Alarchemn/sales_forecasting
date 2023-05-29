33256db6c7afdb85feb50e62fb594913584885ff
Alarchemn <alarchemn@gmail.com>
2023-05-29 13:50:38 -0700

    Fix API

--- CODES/API/models/total_sales.py
+++ CODES/API/models/total_sales.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import re
+from pathlib import Path
+from prophet.serialize import model_from_json
+
+__version__ "0.1.0"
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+
+
 
 def format_prophet(forecast, periods, freq='W-SUN', cvs_name='prediction'):
     """
